c3e7a172b150f5f43a3cd5cceca5effc0cd75787
xtrmntr80 <47057709+xtrmntr80@users.noreply.github.com>
2023-09-07 05:15:10 +0900

    Update script.js
    Purpose: Enable searching across multiple sites.

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const searchInput = document.getElementById('search-input');
 const siteSelection = document.getElementById('site-selection');
 const searchHistory = document.getElementById('search-history');
 
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const siteSelection = document.getElementById('site-selection');
+const searchHistory = document.getElementById('search-history');
+
 searchButton.addEventListener('click', function() {
   const searchTerm = searchInput.value;
-  const selectedSite = siteSelection.value;
-  const query = `site:${selectedSite} ${searchTerm}`;
-  const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+  const selectedSites = Array.from(siteSelection.selectedOptions).map(option => option.value);
   
-  // 履歴機能の追加
-  searchHistory.innerText += `${searchTerm} (${selectedSite}), `;
-  
-  openNewTab(searchUrl);
+  selectedSites.forEach(site => {
+    const query = `site:${site} ${searchTerm}`;
+    const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+    
+    // 履歴機能の追加
+    searchHistory.innerText += `${searchTerm} (${site}), `;
+    
+    openNewTab(searchUrl);
+  });
 });
 
 function openNewTab(url) {
@@ -24,3 +32,5 @@ function openNewTab(url) {
   a.click();
   document.body.removeChild(a);
 }
+
+
