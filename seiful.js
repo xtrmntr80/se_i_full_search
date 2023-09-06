101344e461c0673363c0b75c227d2a1ceceec7a0
xtrmntr80 <47057709+xtrmntr80@users.noreply.github.com>
2023-09-07 02:16:40 +0900

    Update seiful.js

--- seiful.js
+++ seiful.js
@@ -1,19 +1,26 @@
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
+const siteSelection = document.getElementById('site-selection');
+const searchHistory = document.getElementById('search-history');
 
 searchButton.addEventListener('click', function() {
-  const searchTerm = searchInput.value;
-  const query = 'site:asahi.com OR site:yomiuri.co.jp OR site:kyoiku-press.net OR site:nikkei.com OR site:newspicks.com OR site:kyodo.co.jp OR site:nhk.or.jp OR site:kodomo-np.co.jp ' + searchTerm;
-  const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
-  openNewTab(searchUrl);
+  const searchTerm = searchInput.value;
+  const selectedSite = siteSelection.value;
+  const query = `site:${selectedSite} ${searchTerm}`;
+  const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+  
+  // 履歴機能の追加
+  searchHistory.innerText += `${searchTerm} (${selectedSite}), `;
+  
+  openNewTab(searchUrl);
 });
 
 function openNewTab(url) {
-  const a = document.createElement('a');
-  a.href = url;
-  a.target = '_blank';
-  a.rel = 'noopener noreferrer';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  const a = document.createElement('a');
+  a.href = url;
+  a.target = '_blank';
+  a.rel = 'noopener noreferrer';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
